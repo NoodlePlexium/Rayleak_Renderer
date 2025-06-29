6350b89ac3f6e0a1bc50f6dffc541723c3e23664
Nerêns <55632520+NoodlePlexium@users.noreply.github.com>
2025-06-29 10:08:33 +0200

    Changed project name

--- src/main.cpp
+++ src/main.cpp
@@ -49,7 +49,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Luminite Renderer", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Rayleak Renderer", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
